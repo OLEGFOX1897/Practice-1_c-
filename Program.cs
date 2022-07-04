a9b12747d8d4f6ada2d077c28adc771d24fcaf25
oleg1 <oleglapshin1987@gmail.com>
2022-07-04 15:57:13 +0400

    task 2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Input number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine ());
+if (number1>number2)
+     Console.WriteLine ("Max=: "+ number1);
+else if (number1==number2)
+    Console.WriteLine ("You input two same numbers");
+else 
+    Console.WriteLine ("Max=: "+ number2);
+   
