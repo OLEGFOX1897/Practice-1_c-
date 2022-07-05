b72032a40db5eb1369f047938f591d9bc1b605c4
oleg1 <oleglapshin1987@gmail.com>
2022-07-05 11:21:19 +0400

    make comments12

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ else if (number1==number2)
     Console.WriteLine ("You input two same numbers");
 else 
     Console.WriteLine ("Max=: "+ number2);
+
 // Task №4   
 Console.WriteLine ("Input number 1: ");
 int number1 = Convert.ToInt32(Console.ReadLine ());
@@ -20,3 +21,10 @@ int maxNumber=number1;
 if (number2>number1) maxNumber=number2;
 if (number3>number2) maxNumber=number3;
     Console.WriteLine ("Max=: "+ maxNumber);
+
+// Task №6
+Console.WriteLine ("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+if (number%2==0) Console.WriteLine ("Yes. This number share on 2");
+else Console.WriteLine ("No. This number no share on 2");
+
