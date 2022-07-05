d2ae03a52b4c5da4982fc4eea6d8e13554bec088
oleg1 <oleglapshin1987@gmail.com>
2022-07-05 11:19:16 +0400

    make comments1

--- Program.cs
+++ Program.cs
@@ -10,3 +10,13 @@ else if (number1==number2)
 else 
     Console.WriteLine ("Max=: "+ number2);
 // Task №4   
+Console.WriteLine ("Input number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number 2: ");
+int number3 = Convert.ToInt32(Console.ReadLine ());
+int maxNumber=number1;
+if (number2>number1) maxNumber=number2;
+if (number3>number2) maxNumber=number3;
+    Console.WriteLine ("Max=: "+ maxNumber);
