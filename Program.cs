00de8ca699d96d22c83d0fd2831ecce9848544a5
oleg1 <oleglapshin1987@gmail.com>
2022-07-05 11:17:54 +0400

    make comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine ("Input number 1: ");
+﻿// Task №2
+Console.WriteLine ("Input number 1: ");
 int number1 = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine ("Input number 2: ");
 int number2 = Convert.ToInt32(Console.ReadLine ());
@@ -8,4 +9,4 @@ else if (number1==number2)
     Console.WriteLine ("You input two same numbers");
 else 
     Console.WriteLine ("Max=: "+ number2);
-   
+// Task №4   
